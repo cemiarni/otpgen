7c28984efd76f2e95083e1e7714ad0d6a12e2077
Czémán Arnold <czeman.arnold@cloud.bme.hu>
2018-10-06 21:17:09 +0200

    Add rename and list operation

--- otpgen.py
+++ otpgen.py
@@ -1,9 +1,11 @@
 """One-time password generator.
 
 Usage:
+    otpgen list
     otpgen <profile>
     otpgen add <profile> <secret>
     otpgen modify <profile> <secret>
+    otpgen rename <profile> <new_profile>
     otpgen remove <profile>
     otpgen (-h | --help)
     otpgen --version
@@ -16,25 +18,31 @@ The program requires a profile name (<profile>) or a command.
 After a profile was given, the program generates
 a one-time password, that uses the profile's secret.
 You can also give an other command, from below:
+    list      Lists all available profiles.
     add       Adds new profile with a given secret.
     modify    Modifies the secret of the given profile.
+    rename    Renames a given profile.
     remove    Removes the given profile.
 
 """
 
-from docopt import docopt
-from os.path import expanduser
+
 import pyotp
 import os
 import sys
+from docopt import docopt
 
 
-OTP_DIR = os.path.join(expanduser('~'), '.otp')
+OTP_DIR = os.path.join(os.path.expanduser('~'), '.otp')
 
 
 def main():
-    args = docopt(__doc__, version="otpgen 1.1")
-    profile_path = os.path.join(OTP_DIR, args["<profile>"])
+    args = docopt(__doc__, version="otpgen 1.2")
+    if args["<profile>"]:
+        profile_path = os.path.join(OTP_DIR, args["<profile>"])
+    else:
+        profile_path = None
+
     if args["add"] or args["modify"]:
         if not os.path.exists(OTP_DIR):
             os.makedirs(OTP_DIR)
@@ -43,12 +51,23 @@ def main():
     elif args["remove"]:
         if os.path.exists(profile_path):
             os.remove(profile_path)
+    elif args["rename"]:
+        if os.path.exists(profile_path):
+            os.rename(profile_path,
+                      os.path.join(OTP_DIR, args["<new_profile>"]))
+        else:
+            print("The %s profile does not exist!" % args["<profile>"])
+    elif args["list"]:
+        for profile in os.listdir(OTP_DIR):
+            print(profile)
     else:
         if os.path.exists(profile_path):
             with open(profile_path) as f:
                 secret = f.read()
             totp = pyotp.TOTP(secret)
             print(totp.now())
+        else:
+            print("The %s profile does not exist!" % args["<profile>"])
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="otpgen",
-    version="1.1",
+    version="1.2",
     py_modules=['otpgen'],
     install_requires=['pyotp', 'docopt'],
     entry_points={'console_scripts': ['otpgen = otpgen:main']}
